b808e5eb63fe5c9ad48d002808b2cbf3f9fcfeb6
Arnav Bansal <arnavbansal.8232@gmail.com>
2021-06-10 22:01:31 +0530

    header sizing bug fixed

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,47 +6,104 @@ const Headerimg = styled.div`
     background-image: url("./assets/images/illustration-working.svg");
     background-repeat: no-repeat;
     background-size: 130vw;
-    margin-top: 20px;
+    margin-top: 30px;
     margin-left: 30px;
     height: 86vw;
+
+    ${'' /* min-width is intentionally kept 730px  */}
+    @media only screen and (min-width: 730px) {
+        position: absolute;
+        margin: 0;
+        margin-top: 5vw;
+        right: 0;
+        background-size: 70vw;
+        padding: 0;
+        width: 45vw;
+        height: 510px;
+    }
+
+    @media only screen and (min-width: 930px) {
+        background-size: 60vw;
+    }
+
+    @media only screen and (min-width: 1050px) {
+        margin-top: 20px;
+        background-size: 700px;
+    }
 `
 
 const Introduction = styled.div`
-background-color: yellow;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     padding: 0 20px;
-    margin-top: 0;
+    margin-top: 35px;
+
+    @media only screen and (min-width: 720px) {
+        margin-top: 0;
+        order: 1;
+        width: 50%;
+        padding-top: 70px;
+    }
+
+    @media only screen and (min-width: 1250px) {
+        
+    }
 `
 const Headertext = styled.h2`
-margin-top: 0;
+    margin: 0;
     text-align: center;
     color: #232127;
     opacity: 0.9;
     font-size: 2.5rem;
     font-weight: 900;
     text-shadow: 0px 1px, 1px 0px, 1px 1px;
+    line-height: 130%;
+
+    @media only screen and (min-width: 720px) {
+        text-align: left;
+        font-size: 4rem;
+    }
+
+    @media only screen and (min-width: 1250px) {
+        font-size: 5rem;
+    }
 `
 
 const Description = styled.p`
-    margin-top: -25px;
-    margin-bottom: 20px;
+    margin: 0;
     color: #bfbfbf;
     text-align: center;
+    font-weight: 500;
+    margin-top: 5px;
+
+    @media only screen and (min-width: 720px) {
+        text-align: left;
+        font-size: 20px;
+    }
 `
 
 const Start = styled.button`
     color: #fff;
+    background: #2acfcf;
+    cursor: pointer;
+
     font-family: 'Poppins';
     font-weight: 700;
-    font-size: 18px;
+    font-size: 20px;
     border-radius: 50px;
     border: none;
-    background: #2acfcf;
-    padding: 10px 15vw;
-    cursor: pointer;
+    
+    width: 50%;
+    max-width: 200px;
+
+    padding: 15px;
     margin: 0 auto;
+    margin-top: 30px;
+
+    @media only screen and (min-width: 720px) {
+        margin-left: 0;        
+    }
 `
 
 class Header extends Component {
@@ -60,19 +117,17 @@ class Header extends Component {
                 <Navbar />
 
                 <Headerimg />
-                {/* <Headerimg>aa
-                </Headerimg> */}
-                <Introduction>
-                <Headertext>
-                    More than just shorter links
-                </Headertext>
-                <Description>
-                    Build your brand's recognition and get detailed insights on how your links are performing.
-                </Description>
 
-                <Start>
-                    Get Started
-                </Start>
+                <Introduction>
+                    <Headertext>
+                        More than just shorter links
+                    </Headertext>
+                    <Description>
+                        Build your brand's recognition and get detailed insights on how your links are performing.
+                    </Description>
+                    <Start>
+                        Get Started
+                    </Start>
                 </Introduction>
             </React.Fragment>
         );
